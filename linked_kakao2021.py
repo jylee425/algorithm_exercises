d908a361d1c5befdf578f1b71e4ff54952a764ca
JYLee <gimme1dollar@postech.ac.kr>
2021-05-03 19:50:09 +0900

    linked list swapping O(1)

--- linked_kakao2021.py
+++ linked_kakao2021.py
@@ -7,9 +7,10 @@ class Node:
 n = int(input())
 ms = list(map(int, input().split()))
 
-refs = {}
-linked_list = {}
+refs = {} # value -> address
+linked_list = {} # address -> Node (value)
 
+# build linked list
 head = Node(0, None, id(1))
 tail = Node(n+1, id(n), None)
 linked_list[id(0)] = head
@@ -17,7 +18,6 @@ linked_list[id(n+1)] = tail
 for nn in range(n):
     refs[nn+1] = id(nn+1)
     linked_list[id(nn+1)] = Node(nn+1, id(nn), id(nn+2))
-#print(refs)
 
 for m in ms:
   res = ""
@@ -25,17 +25,16 @@ for m in ms:
   m_ = linked_list[refs[m]]
   n_ = linked_list[m_.prev]
   o_ = linked_list[m_.next]
+  head_ = linked_list[head.next]
+  tail_ = linked_list[tail.prev]
   
-  # changing right and left with linked list
+  # swapping right and left with linked list
   if n_ == head:
     m_.next = id(tail.data)
     tail.prev = id(m_.data)
   else:
-    h_ = linked_list[n_.prev]
-    while(h_.prev != id(head.data)):
-      h_ = linked_list[h_.prev]
-    m_.next = id(h_.data)
-    h_.prev = id(m_.data)
+    m_.next = id(head_.data)
+    head_.prev = id(m_.data)
 
     tail.prev = id(n_.data)
     n_.next = id(tail.data)
@@ -44,11 +43,8 @@ for m in ms:
     m_.prev = id(head.data)
     head.next = id(m_.data)
   else :
-    t_ = linked_list[o_.next]
-    while(t_.next != id(tail.data)):
-      t_ = linked_list[t_.next]
-    m_.prev = id(t_.data)
-    t_.next = id(m_.data)
+    m_.prev = id(tail_.data)
+    tail_.next = id(m_.data)
     
     head.next = id(o_.data)
     o_.prev = id(head.data)
