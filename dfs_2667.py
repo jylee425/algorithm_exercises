c2b409b10354f5d049e70b315f4a5d0328a6e0f8
JYLee <gimme1dollar@postech.ac.kr>
2021-06-12 10:31:38 +0900

    fix dfs with global

--- dfs_2667.py
+++ dfs_2667.py
@@ -1,3 +1,18 @@
+def dfs(map, x, y):
+  global count
+  if (x >= 0 and x < N) and (y >= 0 and y < N):
+    if map[x][y] == 1:
+      map[x][y] = 2
+      count += 1
+      dfs(map, x-1, y)
+      dfs(map, x+1, y)
+      dfs(map, x, y-1)
+      dfs(map, x, y+1)
+      return True
+    else:
+      return False
+  return False
+
 from sys import stdin
 
 N = int(input())
@@ -7,30 +22,20 @@ for i in range(N):
   tmp = []
   line = stdin.readline().strip()
   for j in line:
-    tmp.append(j)
+    tmp.append(int(j))
   map.append(tmp)
 
-def dfs(map, x, y):
-  if (x >= 0 and x < N) and (y >= 0 and y < N):
-    if map[x][y] == 1:
-      dfs(x-1, y)
-      dfs(x+1, y)
-      dfs(x, y-1)
-      dfs(x, y+1)
-      return True
-    else:
-      return False
-  return False
 
 result = 0
 answer = []
 count  = 0
 for i in range(N):
   for j in range(N):
-    if dfs(map, i, j) == True:
+    if dfs(map, i, j):
       result += 1
       answer.append(count)
       count = 0
+  
 print(result)
 answer.sort()
 for i in answer:
