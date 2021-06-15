b1c8603cd5786efa07aebb3771a5f4c3fb0c4d9e
JYLee <gimme1dollar@postech.ac.kr>
2021-06-16 00:07:16 +0900

    time limit fixation

--- bfs_11724.py
+++ bfs_11724.py
@@ -1,23 +1,24 @@
-N,M = map(int,input().split())
-adj=[[0]*(N+1)for i in range(N+1)]
+N, M = map(int, input().split())
+adj=[[]*(N+1)for i in range(N+1)]
+visited = [False] * (N+1)
 
 for i in range(M):
-    u,v=map(int,input().split())
-    adj[v][u] = 1
-    adj[u][v] = 1
-visited=[False]*(N+1)
+    u, v = map(int, input().split())
+    adj[u].append(v)
+    adj[v].append(u)
 
 def bfs(v):
     queue=[v]
     while queue:
         v=queue.pop(0)
-        for i in range(1,N+1):
-            if adj[i][v]==1 and not visited[i]:
-                visited[i]=True
-                queue.append(i)
+        for u in adj[v]:
+            if not visited[u]:
+                visited[u]=True
+                queue.append(u)
 
-answer=0
-for i in range(1,N+1):
+count=0
+for i in range(1, N + 1):
     if not visited[i]:
         bfs(i)
-        answer+=1
+        count+=1
+print(count)
