86a7b79d1480784078be556951b2f479119b9b38
JYLee <gimme1dollar@postech.ac.kr>
2023-01-05 19:55:14 +0900

    bruteforce

---
+++ BOJ_15655.py
@@ -0,0 +1,30 @@
+
+def dfs(arr, M, visited, selected):
+    if len(selected) >= M:
+        for s in selected:
+            print(s, end=' ')
+        print()
+        return
+
+    for i, n in enumerate(arr):
+        if i <= visited: continue
+
+        selected.append(n)
+        dfs(arr, M, i, selected)
+        selected.pop()
+
+
+def main(N, M, arr):
+    arr = sorted(arr)
+    visited = -1
+    selected = []
+
+    dfs(arr, M, visited, selected)
+    
+    return
+
+if __name__ == '__main__':
+    N, M = map(int, input().split())
+    arr = list(map(int, input().split()))
+
+    main(N, M, arr)
