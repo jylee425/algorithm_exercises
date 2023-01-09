e0b7d94787abd6aad76294ff6d8bcae9a0d97d93
JYLee <gimme1dollar@postech.ac.kr>
2023-01-10 00:08:15 +0900

    dp

--- BOJ_1309.py
+++ BOJ_1309.py
@@ -8,17 +8,13 @@ def dp(N):
     dp[n][3] = sum over dp[n][i] (i \in {0,1,2})
     """
     
-    memo = [[0] * 4 for _ in range(N+1)]
+    memo = [[0] * 2 for _ in range(N+1)]
     
-    memo[0][3] = 1
-    memo[1][0], memo[1][1], memo[1][2], memo[1][3] = 1, 1, 1, 3
-    # memo[2][0], memo[2][0], memo[2][2], memo[2][3] = 3, 2, 2, 7
+    memo[1][0], memo[1][1] = 1, 1
     for i in range(2, N+1):
-        memo[i][0] = memo[i-1][3]
-        memo[i][1] = memo[i-1][0] + memo[i-1][2]
-        memo[i][2] = memo[i-1][0] + memo[i-1][1]
-        memo[i][3] = memo[i][0] + memo[i][1] + memo[i][2]
-    print(memo[N][3])
+        memo[i][0] = (memo[i-1][0] + memo[i-1][1]*2) % 9901
+        memo[i][1] = (memo[i-1][0] + memo[i-1][1]) % 9901
+    print((memo[N][0] + memo[N][1] * 2)%9901)
 
 if __name__ == '__main__':
     N = int(input())
