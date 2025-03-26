3da31290eedccbb811684863eb913924e86ef776
JYLee <gimme1dollar@postech.ac.kr>
2025-03-25 17:31:05 -0700

    dp

--- BOJ_12865.py
+++ BOJ_12865.py
@@ -1,44 +1,24 @@
-import sys
-
-sys.setrecursionlimit(1e6)
-
-answer = -1
-
-
-def dfs(K, weights, values, curr, packed):
-    global answer
-
-    if curr >= len(weights):
-        return
-
-    # check
-    sum_of_packed_weights, sum_of_packed_values = 0, 0
-    for p in packed:
-        sum_of_packed_weights += weights[p]
-        sum_of_packed_values += values[p]
-
-    if sum_of_packed_weights <= K:
-        if answer == -1 or (answer < sum_of_packed_values):
-            answer = sum_of_packed_values
-    else:
-        return
-
-    # print(curr, packed, answer)
-
-    dfs(K, weights, values, curr + 1, packed + [curr])
-    dfs(K, weights, values, curr + 1, packed)
-    return
-
-
 if __name__ == "__main__":
     N, K = map(int, input().split())
 
-    weights, values = [], []
+    items = [(0, 0)]
     for _ in range(N):
         W, V = map(int, input().split())
-        weights.append(W)
-        values.append(V)
+        items.append((W, V))
+    items.sort(key=lambda x: x[0])  # sort by weight
 
-    # dfs
-    dfs(K, weights, values, curr=0, packed=[])
-    print(answer)
+    # dp
+    dp = [[0] * (K + 1) for _ in range(N + 1)]
+
+    for i in range(1, N + 1):
+        for k in range(1, K + 1):
+            weight, value = items[i]
+
+            if k < weight:
+                dp[i][k] = dp[i - 1][k]
+            else:
+                dp[i][k] = max(dp[i - 1][k], dp[i - 1][k - weight] + value)
+
+        # print(dp[i])
+
+    print(dp[N][K])
